0013d9345aaa92de98c2919dbcff0ba463d0f96d
ilungachris <88008038+ilungachris@users.noreply.github.com>
2025-11-20 20:45:58 +0100

    v18.23

--- server.js
+++ server.js
@@ -775,8 +775,11 @@ app.post('/api/merchant/vouchers/redeem', requireMerchant, async (req, res) => {
     await db.query('BEGIN');
 
 const redeemResult = await db.query(
-  `INSERT INTO redemptions (voucher_id, merchant_id, cashier_id, redeemed_at)
-   VALUES ($1, $2, $3, NOW())
+ // `INSERT INTO redemptions (voucher_id, merchant_id, cashier_id, redeemed_at)
+ // VALUES ($1, $2, $3, NOW())
+ `INSERT INTO redemptions (voucher_id, merchant_id, redeemed_at)
+  
+   VALUES ($1, $2, NOW())
    RETURNING *`,
   [
     voucher.id,
